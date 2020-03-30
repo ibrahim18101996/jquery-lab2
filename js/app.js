6ebd02eeafe6714f9473d99988e1b96aa1a1d2e9
ibrahim18101996 <brhomd.96@gmail.com>
2020-03-31 01:30:16 +0300

    update

--- js/app.js
+++ js/app.js
@@ -8,8 +8,9 @@ $.get('./data/page-1.json')
             console.log(glory1);
             majd.render();
         });
+        renderSelect();
     })
-// keyword.push(glory);
+    // keyword.push(glory);
 
 function glory1(image_url, title, description, keyword, glory) {
     this.image_url = image_url;
@@ -25,22 +26,31 @@ glory1.prototype.render = function () {
 
     let choose = $(`#photo-template`).clone();
     choose.find('h2').text(this.title);
-    // $('main').append(choose);
-
+    choose.attr('class',this.keyword);
     choose.find('img').attr({ 'src': this.image_url, 'width': 270 });
-    // attr('width':150px);
-    $('main').append(choose);
-    // glory1.removeClass();
-
     choose.find('p').text(this.description);
     $('main').append(choose);
+    
+    // keywordArray.push(this.keyword);
+}
+$('#selection').on('change',function(){
+    for (let i=0;i<unReeatArray.length;i++){
+        if ($('#selection').val()==unReeatArray[i]){
+            $('section').hide();
+            $(`.${unReeatArray[i]}`).show();
+        }
+    }
+})
 
-    keywordArray.push(this.keyword);
+
+    function renderSelect(){
     for (let i = 0; i < keywordArray.length; i++) {
         if (unReeatArray.indexOf(keywordArray[i]) === -1) {
             unReeatArray.push(keywordArray[i]);
-            let option = $('option');
-            $('select').append(`<option value ='${unReeatArray[i]}'>${unReeatArray[i]}</option>`);
         }
     }
-}
+        for (let i=0;i<unReeatArray.length;i++){
+            $('select').append(`<option value ='${unReeatArray[i]}'>${unReeatArray[i]}</option>`);
+            
+        }
+    }    
