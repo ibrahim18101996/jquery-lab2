e65e8e35d007966c3f27d2cdd9ffd8c5afd39346
ibrahim18101996 <brhomd.96@gmail.com>
2020-04-01 00:01:14 +0300

    do the Mustache

--- js/app.js
+++ js/app.js
@@ -2,46 +2,68 @@
 let ibrahim;
 $.get('./data/page-1.json')
     .then(galeryOfHorns => {
-        console.log(galeryOfHorns)
         galeryOfHorns.forEach((val) => {
             let ibrahim = new Horns(val.image_url, val.title, val.description, val.keyword, val.horns);
-            ibrahim.render();
+            ibrahim.printOut();
         });
         renderSelect()
-        $('.hide').remove()
     });
+$.get('./data/page-2.json')
+    .then(page2 => {
+    page2.forEach((val) => {
+        let majd = new Page2(val.image_url, val.title, val.description, val.keyword, val.horns);
+        majd.printOut();
+    });
+    renderSelect()
+});
 function Horns(image_url, title, description, keyword, horns) {
     this.image_url = image_url;
     this.title = title;
     this.description = description;
     this.keyword = keyword;
     this.horns = horns;
-    keywordArray.push(this.keyword)
+    keywordArray.push(this.keyword);
 }
-var keywordArray = []
-let unRepeatArray = []
-let p;
-Horns.prototype.render = function () {
-    let choose = $('#photo-template').clone();
-    choose.find('h2').text(this.title);
-    choose.attr('class', this.keyword)
-    choose.find('img').attr({ 'src': this.image_url, 'width': 300, 'class': 'cssClass', 'hight': 250 });
-    choose.find('p').text(this.description);
-    $('main').append(choose);
-    // keywordArray.push(this.keyword)
+function Page2(image_url, title, description, keyword, horns) {
+    this.image_url = image_url;
+    this.title = title;
+    this.description = description;
+    this.keyword = keyword;
+    this.horns = horns;
+    keywordArray.push(this.keyword);
 }
-$('#selectEle').on('change', function () {
-    console.log($('#selectEle').val())
-    for (let i = 0; i < unRepeatArray.length; i++) {
-        if ($('#selectEle').val() == unRepeatArray[i]) {
-            $(`section`).hide()
-            $(`.${unRepeatArray[i]}`).show()
-        }
-        if ($('#selectEle').val() == unRepeatArray[i]) {
+var keywordArray = [];
+let unRepeatArray = [];
+Horns.prototype.printOut = function () {
 
+    let choose = $('#template').html();
+    let html = Mustache.render(choose, this)
+    console.log(choose);
+    let sections = $('#photo-template').append(html);
+    console.log(sections);
+    $('main').append(sections);
+}
+Page2.prototype.printOut = function () {
+
+
+    let choose = $('#template').html();
+    let html = Mustache.render(choose, this);
+    console.log(choose);
+    let sections2 = $('#photo-template-page2').append(html);
+    console.log(sections2);
+    $('final').append(sections2);
+}
+$(`div`).hide();
+$('#selectEle').on('change', function () {
+    console.log($('#selectEle').val());
+    for (let i = 0; i < unRepeatArray.length; i++) {
+        if ($(`#selectEle`).val() == unRepeatArray[i]) {
+            $(`div`).hide();
+            console.log($(`div`).hide());
+            $(`.${unRepeatArray[i]}`).show();
         }
         if ($('#selectEle').val() == 'default') {
-            $(`section`).show()
+            $(`div`).show();
         }
     }
 })
@@ -55,6 +77,8 @@ function renderSelect() {
     }
     for (let i = 0; i < unRepeatArray.length; i++) {
         $('select').append(`<option value ='${unRepeatArray[i]}'>${unRepeatArray[i]}</option>`);
-        console.log('mmmmm  ' + unRepeatArray[i])
     }
 }
+$('button').on('click', function () {
+    console.log(this);
+})
